35e26ed8487165b0f16baa0f33945dd35cb01514
tatoguima2004 <75127525+tatoguima2004@users.noreply.github.com>
2022-11-07 08:45:48 -0300

    Update insert_dasilva.sql

--- insert_dasilva.sql
+++ insert_dasilva.sql
@@ -5,3 +5,23 @@
     RotacionTaller
     Alumno
 */
+Curso:
+INSERT INTO materias VALUES ('Lengua y Literatura');
+INSERT INTO materias VALUES ('Matematica');
+INSERT INTO materias VALUES ('Fisica');
+INSERT INTO materias VALUES ('Educacion Fisica');
+INSERT INTO materias VALUES ('Historia');
+INSERT INTO materias VALUES ('Economia');
+INSERT INTO materias VALUES ('Base de datos');
+INSERT INTO materias VALUES ('Dibujo Tecnico');
+INSERT INTO materias VALUES ('Redes');
+INSERT INTO materias VALUES ('Algoritmos');
+
+RotacionTaller:
+
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000003','4','3')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000002','5','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','2','5')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('11001100','1','7')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00110011','6','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','4','1')
